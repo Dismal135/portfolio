f298b2461270b9b2a7600a1a1d266c919179aee1
PHYO THI HA <phyothiha97531@gmail.com>
2024-11-25 21:28:12 +0630

    portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,18 @@
+import AboutMe from "@/components/aboutMe/aboutMe";
 import Clock from "@/components/clock/clock";
+import Penguin from "@/components/penguin/penguin";
 import { Button } from "@nextui-org/button";
 import Link from "next/link";
 
 export default function Portfolio () {
     return (
-        <>
+        <div className="p-2">
         <Clock />
-        <Button size="md" className="absolute left-1 top-1" as={Link} href="/">Back</Button>
-        </>
+        <Button size="md" className="ml-1 mt-1" as={Link} href="/">Back</Button>
+        <main className="flex flex-col md:flex-row justify-center items-center">
+        <Penguin />
+        <AboutMe />
+        </main>
+        </div>
     )
 }
---
+++ src/components/aboutMe/aboutMe.tsx
@@ -0,0 +1,26 @@
+export default function AboutMe () {
+    return (
+        <div className="space-y-2">
+            <div>
+            <h1>PHYO THI HA</h1>
+            </div>
+            <div>
+            <h1>Hi! I am a Mid Level Full Stack Developer.</h1>
+            <p>My current skills are html, css, javascript, reactJs, reactNative, nextJs, nodeJs, sql and mongoDb.</p>
+            </div>
+            <div>
+            <h1 className="bg-black text-white w-fit">Experience</h1>
+            <p>Myanmar Media Linkage 1 year 2023-2024</p>
+            <p>Freelance : 1 year 3 projects</p>
+            </div>
+            <div>
+            <h1 className="bg-black text-white w-fit">Certificates</h1>
+            <p>Responsive Web Design</p>
+            <p>JavaScript Algorithms and Data Structures</p>
+            <p>Front End Development Libraries</p>
+            <p>Data Visualization</p>
+            <p>Back End Development and APIs</p>
+            </div>
+        </div>
+    )
+}
