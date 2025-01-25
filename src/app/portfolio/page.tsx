e230871ffa5f734260bd3443932beb958403e960
PHYO THI HA <phyothiha97531@gmail.com>
2025-01-25 13:16:05 +0630

    add

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -20,15 +20,21 @@ export default function Portfolio () {
         </div>
         ) :
 
-        <div className="flex flex-col gap-10 justify-center items-center h-[100vh] bg-black text-white font-bold">
+        <div className="flex flex-col gap-10 justify-center items-center h-[100vh] bg-black text-white font-bold sm:flex-row">
             <Image alt="profile" src={profile} className="rounded-[50%] border border-white w-[100px] h-[100px]" />
             <div  className="w-[300px] flex flex-col gap-2">
             <h1 className="font-normal">Name - Phyo Thiha</h1>
-            <h1>Frontend - <span className="font-normal">html, css, javascript, react, nextjs, tailwind, bootstrap, jquery etc...</span></h1>
-            <h1>Backend - <span className="font-normal">nodejs, expressjs, python, nextjs</span></h1>
-            <h1>Database - <span className="font-normal">sqlite, mysql, mongodb, either relational or document object model databases</span></h1>
-            <h1>Auth - <span className="font-normal">passportjs, authjs, etc...</span></h1>
+            <h1>Frontend - <span className="font-normal">html, css, javascript, reactjs, vuejs, angularjs, nextjs, tailwind, bootstrap, jquery, ajax</span></h1>
+            <h1>Backend - <span className="font-normal">nodejs, express, python, php</span></h1>
+            <h1>Database - <span className="font-normal">sqlite, mysql, mongodb, postgres</span></h1>
+            <h1>Auth - <span className="font-normal">passportjs, authjs</span></h1>
             <h1>Version control - <span className="font-normal">git, github</span></h1>
+            <h1>Deployment - <span className="font-normal">docker containerization</span></h1>
+            </div>
+            <div className="flex flex-col gap-2">
+                <h1>General Knowledge</h1>
+                <h1>Operating System - <span className="font-normal">windows, linux</span></h1>
+                <h1>Technical Skills - <span className="font-normal">Comptia A+, NE</span></h1>
             </div>
         </div>
     )
